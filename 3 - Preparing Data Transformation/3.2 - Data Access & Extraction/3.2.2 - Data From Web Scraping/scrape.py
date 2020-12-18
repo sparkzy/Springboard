d929799518aafb87b9810a2b5da86c2a1c1b6ba7
sparkzy <31968082+sparkzy@users.noreply.github.com>
2020-12-17 21:13:08 -0500

    completed exercise 3.2.2

---
+++ 3 - Preparing Data Transformation/3.2 - Data Access & Extraction/3.2.2 - Data From Web Scraping/scrape.py
@@ -0,0 +1,39 @@
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup 
+
+my_url = 'https://www.newegg.com/p/pl?d=graphics+card'
+
+# opening connection, grabbing the page
+uClient = uReq(my_url)
+page_html = uClient.read()
+uClient.close()
+
+# html parser - traverse DOM ex: page_soup.h, page_soup.p, page_soup.b.span.a
+page_soup = soup(page_html, 'html.parser')
+
+# grabs each product
+containers = page_soup.findAll('div', {'class':'item-cell'})
+
+filename = 'products.csv'
+f = open(filename, 'w')
+
+headers = 'brand, product_name, shipping \n'
+
+f.write(headers)
+
+for container in containers:
+    brand = container.div.div.a.img['title']
+
+    title_container = container.findAll('a', {'class':'item-title'})
+    product_name = title_container[0].text
+
+    shipping_container = container.findAll('li', {'class':'price-ship'})
+    shipping = shipping_container[0].text.strip()
+
+    print('brand: ' + brand)
+    print('brand: ' + product_name)
+    print('brand: ' + shipping)
+
+    f.write(brand + ', ' + product_name.replace(',', '|') + ', ' + shipping + '\n')
+    
+f.close()
